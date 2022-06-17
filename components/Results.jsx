91409ebbf6d5bf941f006ef9e66a73113e9f6858
anohene1 <anoheneisaac@gmail.com>
2022-06-17 20:15:05 +0000

    render movies

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,5 +1,16 @@
-function Results(props) {
-  return <div></div>;
+import Thumbnail from "./Thumbnail";
+
+function Results({ results }) {
+  return (
+    <div
+      className="px-5 my-10 sm:grid md:grid-cols-2
+    lg:grid-cols-3 3xl:flex flex-wrap justify-center"
+    >
+      {results.map((result) => (
+        <Thumbnail key={result.id} result={result} />
+      ))}
+    </div>
+  );
 }
 
 export default Results;
